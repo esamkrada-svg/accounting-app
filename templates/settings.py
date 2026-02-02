3d9c25c8106e4cc519586a0060249794dec2a85a
esamkrada-svg <esam.krada@gmail.com>
2026-02-02 03:49:14 +0300

    Add app settings and language translations
    Added application settings for name and default language with language translations.

--- templates/settings.py
+++ templates/settings.py
@@ -1,1 +1,21 @@
+# app/settings.py
 
+APP_NAME = "Accounting System"
+DEFAULT_LANGUAGE = "ar"  # ar / en
+
+LANGUAGES = {
+    "ar": {
+        "accounts": "دليل الحسابات",
+        "journal": "القيود اليومية",
+        "persons": "الأشخاص",
+        "add": "إضافة",
+        "save": "حفظ",
+    },
+    "en": {
+        "accounts": "Chart of Accounts",
+        "journal": "Journal Entries",
+        "persons": "Persons",
+        "add": "Add",
+        "save": "Save",
+    }
+}
